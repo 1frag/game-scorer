9f1c9e237d5f632758c9ce9bab2873c6829c871f
Aleksey Piskunov <piskunov.alesha@gmail.com>
2025-09-28 16:55:32 +0300

    add on_startup

--- server.py
+++ server.py
@@ -3,9 +3,13 @@ import typing
 import aiohttp
 import dataclasses
 import json
+import re
+import os
 import enum
 
 from aiohttp import web
+
+PORT = 9000
 
 
 async def view_handler(request):
@@ -84,13 +88,23 @@ async def data_handler(request):
     return web.json_response(res)
 
 
+async def on_startup(app):
+    try:
+        host = re.compile('[\d.]+/24').findall(os.popen('ip a | grep /24').read())[0][:-3]
+        url = f'http://{host}:{PORT}'
+        print(f'\nOpen {url}/v and let {url}/s for others\n')
+    except Exception as e:
+        print('failed to get address', e)
+
+
 def main():
     app = web.Application()
     app.router.add_route('GET', '/v', view_handler)
     app.router.add_route('GET', '/s', setter_handler)
     app.router.add_route('POST', '/set', set_handler)
     app.router.add_route('GET', '/data', data_handler)
-    web.run_app(app, port=9000)
+    app.on_startup.append(on_startup)
+    web.run_app(app, port=PORT)
 
 
 if __name__ == '__main__':
